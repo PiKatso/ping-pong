5c155c0000a5afa1f6f311479ff104eb9569b977
Kat Gifford <katgiff2007@gmail.com>
2017-03-31 09:06:59 -0700

    add first specification, only excepting whole numbers, no characters or symbols

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
 //Business Logic//
+var splitToNumbers = function(number){
+  var inputNumbers = [];
+
+}
+
+number = math.floor(input)
 
 
 
 
 
 
-
-
-
+function validateinput(number) {
+  return ! /[^\d]/.test(number);
+}
 
 //UI Logic//
+$(document).ready(function(){
+  $('form').submit(function(){
+    var input = $("input").val();
+    if validatedInput(input) === true {
+      return $('output').text(results);
+    }
+    return false;
+  });
+});
